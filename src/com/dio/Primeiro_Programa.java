abc5b556eac716a6cce01e635d37674847c283db
DAVIDRODSI <david.rodrygues@hotmail.com>
2021-10-11 17:07:33 -0300

    exercicio

---
+++ src/com/dio/Primeiro_Programa.java
@@ -0,0 +1,19 @@
+package com.dio;
+
+import com.dio.model.gato;
+
+public class Primeiro_Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		gato gato = new gato();
+		
+		
+		int a = 1;
+		int b = 2;
+		int soma = a + b;
+		System.out.println("Hello Word " + soma);
+	}
+
+}
